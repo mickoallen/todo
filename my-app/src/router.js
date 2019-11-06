ac86f2b65d30c84134c39a4109026d7ad8e8def3
mick <mick@viafoura.com>
2019-11-06 08:29:08 -0500

    small fixes

--- my-app/src/router.js
+++ my-app/src/router.js
@@ -5,7 +5,6 @@ import TodosList from "./components/TodosList.vue";
 Vue.use(Router);
 
 export default new Router({
-  mode: "history",
   routes: [
     {
       path: "/",
--- src/main/java/com/mo/todo/TodoApplication.java
+++ src/main/java/com/mo/todo/TodoApplication.java
@@ -2,12 +2,26 @@ package com.mo.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TodoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
+	}
+
+	/**
+	 * this isnt ideal but helps for local testing - lookup webpack dev server proxy instead!
+	 */
+	@Configuration
+	public class Config implements WebMvcConfigurer {
+		@Override
+		public void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/**").allowedOrigins("*");
+		}
 	}
 
 }
