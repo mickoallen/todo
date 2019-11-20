86e260c1cca04afa2361422c999d53ab7f0c3eb3
mohamed.nasar <mohamed.nasar@paytm.com>
2019-11-20 17:13:25 -0500

    build

--- src/main/java/com/mo/todo/TodoApplication.java
+++ src/main/java/com/mo/todo/TodoApplication.java
@@ -1,16 +1,26 @@
 package com.mo.todo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Properties;
+
+@EnableAutoConfiguration
 @SpringBootApplication
 public class TodoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TodoApplication.class, args);
+		Properties properties = new Properties();
+		SpringApplication app =
+				new SpringApplication(TodoApplication.class);
+		properties.setProperty("spring.resources.static-locations",
+				"file:/app/my-app/");
+		app.setDefaultProperties(properties);
+		app.run(args);
 	}
 
 	/**
